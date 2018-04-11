65f209f0cc98df664c12583d5317742da156a79e
aaakkk6060 <aaakkk6060@126.com>
2018-04-12 01:19:10 +0800

    CVS对方水电费水电费水电费是

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import com.ray.StringU;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here sdfs dfCVS对方水电费
+	// write your code here sdfs dfCVS对方水电费dd
         System.out.println(StringU.convertHanzi2Pinyin("刘霖",false));
     }
 }
