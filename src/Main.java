22fcd6f6532b5556382a223c7f8650c3cce8fe6b
aaakkk6060 <aaakkk6060@126.com>
2018-04-12 01:54:12 +0800

    ssssssssssssssssssss

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write菜刀队
-        System.out.println(StringU.convertHanzi2Pinyin("刘霖",false));
+        System.out.println(StringU.convertHanzi2Pinyin("FFFFFFFFFFFF",false));
     }
 }
